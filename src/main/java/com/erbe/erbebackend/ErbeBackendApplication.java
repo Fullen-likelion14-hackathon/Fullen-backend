ecd38a2eee667cf96fa7ac04fefc92ee445541a4
wjdah1028 <wjdah1028@naver.com>
2026-08-03 17:55:32 +0900

    :tada: Start: Erbe 백엔드 프로젝트 생성

---
+++ src/main/java/com/erbe/erbebackend/ErbeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.erbe.erbebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErbeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErbeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erbe/erbebackend/ErbeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erbe.erbebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErbeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
