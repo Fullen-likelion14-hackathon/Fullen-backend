1e7836d9102c536bfd1d2b21c07431493348f949
eunhasu03 <softabc12@gmail.com>
2026-08-07 00:35:39 +0900

    🐛 Fix: Photo seq순서대로 가져오도록 수정 (#14)

--- src/main/java/com/erbe/erbebackend/domain/photo/repository/PhotoRepository.java
+++ src/main/java/com/erbe/erbebackend/domain/photo/repository/PhotoRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
-    List<Photo> findAllByPost(Post post);
+    List<Photo> findAllByPostOrderBySeqAsc(Post post);
 }
