c613424f3d9e8bb3857048c225a1d1fc21cffb29
wjdah1028 <wjdah1028@naver.com>
2026-08-08 20:06:19 +0900

    :recycle: Refactor: 필드 제거 및 테이블 이름 추가, BaseTimeEntity 상속으로 변경

--- src/main/java/com/erbe/erbebackend/domain/patch/entity/Patch.java
+++ src/main/java/com/erbe/erbebackend/domain/patch/entity/Patch.java
@@ -1,43 +1,31 @@
 package com.erbe.erbebackend.domain.patch.entity;
 
-import com.erbe.erbebackend.domain.artist.entity.Artist;
 import com.erbe.erbebackend.domain.patch.enums.PatchType;
-import com.erbe.erbebackend.domain.photo.entity.Photo;
 import com.erbe.erbebackend.domain.user.entity.User;
+import com.erbe.erbebackend.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDate;
-
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Patch {
+@Table(name = "patches")
+public class Patch extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
     private PatchType type;
 
-    private LocalDate createdAt;
+    @Column(nullable = false, length = 300)
+    private String imgUrl; // 커스텀 패치 이미지 url
 
-    private String imgUrl;
-
-    @ManyToOne
-    @JoinColumn(name = "artist_id")
-    private Artist artist;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
-    @OneToOne
-    @JoinColumn(name = "photo_id")
-    private Photo photo;
 }
