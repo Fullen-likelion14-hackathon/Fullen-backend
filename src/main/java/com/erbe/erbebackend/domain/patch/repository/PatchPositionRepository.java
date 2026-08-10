8ed5cd1c4350dec86231426a1deedb9d34ba5ff7
wjdah1028 <wjdah1028@naver.com>
2026-08-10 16:43:06 +0900

    :recycle: Refactor: 패치 주문 관련 메서드 추가

--- src/main/java/com/erbe/erbebackend/domain/patch/repository/PatchPositionRepository.java
+++ src/main/java/com/erbe/erbebackend/domain/patch/repository/PatchPositionRepository.java
@@ -1,6 +1,7 @@
 package com.erbe.erbebackend.domain.patch.repository;
 
 import com.erbe.erbebackend.domain.bag.entity.UserBag;
+import com.erbe.erbebackend.domain.order.entity.PatchOrder;
 import com.erbe.erbebackend.domain.patch.entity.Patch;
 import com.erbe.erbebackend.domain.patch.entity.PatchPosition;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,4 +15,10 @@ public interface PatchPositionRepository extends JpaRepository<PatchPosition, Lo
 
     // 패치가 가방에 달려있는지 조회
     boolean existsByPatch(Patch patch);
+
+    // 가방에서 주문이 안된 패치 위치 조회
+    List<PatchPosition> findAllByUserBagAndIsEditableTrue(UserBag userBag);
+
+    // 패치 주문에 포함된 패치 위치 조회
+    List<PatchPosition> findAllByPatchOrder(PatchOrder patchOrder);
 }
